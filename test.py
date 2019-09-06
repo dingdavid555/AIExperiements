778a5e3afab30f6d1faa293c1b0581404c9a2e3f
Yuhao (David) Ding <1dingdav1@gmail.com>
2019-09-05 20:37:15 -0400

    First Commit, add two examples from online lessons

---
+++ test.py
@@ -0,0 +1,63 @@
+# LINEAR REGRESSION EXAMPLE
+
+# IMPORTS ==========
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import  style
+
+# IMPORTING AND MANIPULATING DATA
+data = pd.read_csv("student-mat.csv", sep=";")
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+
+
+# INITIALIZING VARIABLES
+predict = "G3"
+x = np.array(data.drop([predict], 1))
+y = np.array(data[predict])
+best = 0
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+
+
+for _ in range(10000):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+
+    linear = linear_model.LinearRegression()
+
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+
+    print(acc)
+    if acc > best:
+        best = acc
+        with open("studentmodel.pickle", "wb") as f:
+            pickle.dump(linear, f)
+
+print("Best trial is: ", best)
+
+
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
+
+
+# output
+print("Co: \n", linear.coef_)
+print("Intercept: \n", linear.intercept_)
+
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
+
+p = "absences"
+style.use("ggplot")
+
+pyplot.scatter(data[p], data["G3"])
+
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
